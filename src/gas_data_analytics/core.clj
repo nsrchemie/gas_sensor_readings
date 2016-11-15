19e59967f84022c26fdffc6884f9b4c9bc828cf8
nsrchemie <nsrchemie@gmail.com>
2016-11-14 19:03:43 -0500

    simple ring-jetty server works and sends data to browser via http

--- src/gas_data_analytics/core.clj
+++ src/gas_data_analytics/core.clj
@@ -1,10 +1,19 @@
 (ns gas-data-analytics.core
 (:require [clojure.java.io :refer [as-url]])
-(:require [ring.adapter.jetty :refer [run-jetty]])
-)
+(:require [ring.adapter.jetty :refer [run-jetty]]))
 
 (defn -main []
-	(def raw (slurp (as-url "http://10.0.0.240:8080")))
+(def raw (slurp (as-url "http://10.0.0.240:8080")))
 			(def cleaned_sequences (map (fn[x] (Integer/parseInt x)) (re-seq #"[1-9][0-9]*|0" raw)))
-			(prn (take 10 cleaned_sequences))
-			)
+			(def readings (take 10 cleaned_sequences))
+			
+(defn handler "request -> response"
+	[req]
+	{:status 200
+	:headers {"Content-Type" "text/plain; charset=UTF-8"}
+	:body readings})
+
+(defonce server
+	(run-jetty handler {:port 3000
+		:join? false}))
+)
