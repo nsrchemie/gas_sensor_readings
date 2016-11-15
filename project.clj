9e0be8cef5a8d970f7d0dc77b72f126882fe8ee7
nsrchemie <nsrchemie@gmail.com>
2016-11-14 21:30:32 -0500

    Got working xchart xy plot

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+[aysylu/loom "0.6.0"]
+[com.hypirion/clj-xchart "0.2.0"]]
   :main gas-data-analytics.core)
--- src/gas_data_analytics/core.clj
+++ src/gas_data_analytics/core.clj
@@ -1,9 +1,15 @@
 (ns gas-data-analytics.core)
 
 (require '[clojure.java.io :refer [as-url]])
+(require '[com.hypirion.clj-xchart :as c])
 
 (defn -main []
 	(def raw (slurp (as-url "http://10.0.0.240:8080")))
 			(def cleaned_sequences (map (fn[x] (Integer/parseInt x)) (re-seq #"[1-9][0-9]*|0" raw)))
 			(prn (take 10 cleaned_sequences))
+			(def chart 
+				(c/xy-chart {"gas Concentration fluctuations" 
+					[(drop 10 cleaned_sequences) (take 10 cleaned_sequences) ]}))
+			
+			(c/view chart)
 			)
