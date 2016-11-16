db93cf529ea9cfd344d79f77d6989113c0e1019c
nsrchemie <nsrchemie@gmail.com>
2016-11-16 18:11:41 -0500

    working on slurp compatibility

--- src/gas_data_analytics/core.clj
+++ src/gas_data_analytics/core.clj
@@ -7,9 +7,13 @@
 	(def raw (slurp (as-url "http://10.0.0.240:8080")))
 			(def cleaned_sequences (map (fn[x] (Integer/parseInt x)) (re-seq #"[1-9][0-9]*|0" raw)))
 			(prn (take 10 cleaned_sequences))
+
+(defstruct xyp :x :y :stroke)			
+
+
 			(def chart 
 				(c/xy-chart {"gas Concentration fluctuations" 
-					[(drop 10 cleaned_sequences) (take 10 cleaned_sequences) ]}))
+					[(drop 100 cleaned_sequences) (take 100 cleaned_sequences) ]}))
 			
 			(c/view chart)
 			)
