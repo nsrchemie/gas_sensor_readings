6d37937d9254de773eeb14956502cdcefb9fed95
nsrchemie <nsrchemie@gmail.com>
2016-11-16 18:22:20 -0500

    finished formatting map data

--- src/gas_data_analytics/core.clj
+++ src/gas_data_analytics/core.clj
@@ -5,7 +5,9 @@
 (defn -main []
 	(def raw (slurp (as-url "http://10.0.0.240:8080")))
 			(def cleaned_sequences (map (fn[x] (Integer/parseInt x)) (re-seq #"[1-9][0-9]*|0" raw)))
-			(prn (take 10 cleaned_sequences))
+			; (prn (take 10 cleaned_sequences))
 
 (defstruct xyp :x :y :stroke)
+(doseq [[time sensor] ((fn[x] (map vector (range) x)) (take 10 cleaned_sequences))] (println (struct xyp time sensor "a")))
+
 			)
