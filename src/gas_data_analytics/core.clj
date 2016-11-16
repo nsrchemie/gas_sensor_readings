9d3c58de9080d270a64c7b9f291733a1d8a4900e
nsrchemie <nsrchemie@gmail.com>
2016-11-16 18:15:07 -0500

    adding map struct for formatting data for potential js chartify library use

--- src/gas_data_analytics/core.clj
+++ src/gas_data_analytics/core.clj
@@ -6,4 +6,6 @@
 	(def raw (slurp (as-url "http://10.0.0.240:8080")))
 			(def cleaned_sequences (map (fn[x] (Integer/parseInt x)) (re-seq #"[1-9][0-9]*|0" raw)))
 			(prn (take 10 cleaned_sequences))
+
+(defstruct xyp :x :y :stroke)
 			)
