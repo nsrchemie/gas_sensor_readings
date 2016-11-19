1d80931c0e2edad252bf0a232b67422628f79bb2
nsrchemie <nsrchemie@gmail.com>
2016-11-19 15:53:42 -0500

    added commented out code for retaining time data

--- src/gas_data_analytics/core.clj
+++ src/gas_data_analytics/core.clj
@@ -7,8 +7,12 @@
 			(def cleaned_sequences (map (fn[x] (Integer/parseInt x)) (re-seq #"[1-9][0-9]*|0" raw)))
 			; (prn (take 10 cleaned_sequences))
 
+;If I want to keep the RSPI time data
+; (def split_data (/ (count cleaned_sequences) 2)
+
+
 ; (defstruct xyp :x :y :stroke)
 ; (doseq [[time sensor] ((fn[x] (map vector (range) x)) (take 10 cleaned_sequences))] (println (struct xyp time sensor "a")))
 (doseq [[time sensor] ((fn[x] (map vector (range) x)) 
-(take 10 cleaned_sequences))] (println time, " ", sensor))
+(take 10 cleaned_sequences))] (println "Time: " time, "  ADC: ", sensor))
 			)
